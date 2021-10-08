b5e489fe885d6b3977bf68dd7a5b0843d51c4aab
Jorman Israel <84195577+jisrael-R@users.noreply.github.com>
2021-10-08 13:54:41 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -36,13 +36,13 @@ function generateMarkdown(data) {
   
   ## Questions:
   - Github: [${data.github}](https://github.com/${data.github})
-  - Email: ${data.email} `;
-  
+  - Email: ${data.email}
  
   #
   
  
-  *this file was generated with this project*
+  *this file was generated with this project* `;
+  
 }
 
 module.exports = generateMarkdown;
